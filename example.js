7d17227c806a7c31bd2e2b9b252acb2a91acf147
serafina <parasitism@protonmail.com>
2020-10-19 22:17:22 -0700

    Auto commit + push. Mon Oct 19 22:17:22 PDT 2020

---
+++ example.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+require('yargs')
+  .scriptName("arena-chan-dl")
+  .usage('$0 <cmd> [args]')
+  .command('hello [name]', 'welcome ter yargs!', (yargs) => {
+    yargs.positional('name', {
+      type: 'string',
+      default: 'frog',
+      describe: 'the name to say hello to'
+    })
+  }, function (argv) {
+    console.log('hello', argv.name, 'welcome to yargs!')
+  })
+  .help()
+  .argv
