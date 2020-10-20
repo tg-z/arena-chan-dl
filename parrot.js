1064b580266240885c9269674a8d71ddca391cd7
serafina <parasitism@protonmail.com>
2020-10-19 22:38:02 -0700

    Auto commit + push. Mon Oct 19 22:38:02 PDT 2020

---
+++ parrot.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+require('yargs')
+  .scriptName("pirate-parser")
+  .usage('$0 <cmd> [args]')
+  .command('hello [name]', 'welcome ter yargs!', (yargs) => {
+    yargs.positional('name', {
+      type: 'string',
+      default: 'Cambi',
+      describe: 'the name to say hello to'
+    })
+  }, function (argv) {
+    console.log('hello', argv.name, 'welcome to yargs!')
+  })
+  .help('help').alias('help', 'h')
+  .argv
